7663c2466c372786cece24c3d3ff3a2d8906135c
ztq35795 <1014472410@qq.com>
2014-12-03 19:23:17 +0000

    开始研究如何返回静态网页

--- server.py
+++ server.py
@@ -8,6 +8,20 @@ print('starting up on %s port %d' % server_address,file=sys.stderr)
 sock.bind(server_address)
 sock.listen(1)
 
+content=b'''HTTP/1.x 200 OK
+Content-Type:text/html
+
+<head>
+<title>WOW</title>
+</head>
+<html>
+<p>Wow, Python Server</p>
+</html>>
+'''
+#f = open('index.html','rb')
+#content = content + f.read()
+#f.close()
+
 while True:
     print('waitong for a connection',file=sys.stderr)
     connection,client_address = sock.accept()
@@ -16,13 +30,10 @@ while True:
         print('connection from',client_address,file=sys.stderr)
 
         while True:
-            data = connection.recv(16)
-            print('received "%s"' %data)
+            data = connection.recv(1024)
             if data:
-                print('sending data back to the client',file=sys.stderr)
-                connection.sendall(data)
+                connection.sendall(content)
             else:
-                print('no data from',client_address,file=sys.stderr)
                 break
     finally:
         connection.close()
