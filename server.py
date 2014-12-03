e6d05e96f817618aaef044f3492c3c7fba4b1f46
ztq35795 <1014472410@qq.com>
2014-12-03 21:27:58 +0000

    完成404 Not Found

--- server.py
+++ server.py
@@ -1,45 +1,84 @@
-import socket
-import sys
+import socketserver
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_address = ('localhost',10000)
-#This is not a bug
-print('starting up on %s port %d' % server_address,file=sys.stderr)
-sock.bind(server_address)
-sock.listen(3)
 
-content=b'''HTTP/1.x 200 OK
-Content-Type:text/html
+class MYTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
 
-'''
-f = open('index.html','rb')
-t_content = content + f.read()
-f.close()
+        tool=CreateAndJudge()
+        request = self.request.recv(1024).decode('UTF-8')
 
-content=b'''HTTP/1.x 200 OK
-Content-Type:image/jpg
-
-'''
-f = open('arch.jpg','rb')
-p_content = content + f.read()
-f.close
-
-while True:
-    connection,client_address = sock.accept()
-    request = connection.recv(1024).decode('UTF-8')
-    method = request.split(' ')[0]
-    src = request.split(' ')[1]
-
-    if method == 'GET':
-        print('Connected by',client_address)
+        print('Connected by',self.client_address)
         print('Request is',request)
 
-        if src == '/arch.jpg':
-            outputcontent=p_content
+        method = request.split(' ')[0]
+        src = request.split(' ')[1]
+
+        if method == 'GET':
+            if tool.isImageRequested(src):
+                OutPutConcent = tool.GetImage()
+
+            elif tool.isIndexRequested(src):
+                OutPutConcent = tool.GetIndex()
+
+            else:
+                OutPutConcent = tool.Get404Page()
+
+            self.request.sendall(OutPutConcent)
+
+class CreateAndJudge():
+    def __init__(self):
+        self.t_content = b'''HTTP/1.x 200 OK
+        Content-Type:text/html
+
+        '''
+
+        self.p_content = b'''HTTP/1.x 200 OK
+        Content-Type:image/jpg
+
+        '''
+
+        self.NotFoundContent = b'''HTTP/1.x 404 Not Found
+        Content-Type:text/html
+
+        '''
+
+    def GetIndex(self):
+        f = open('index.html','rb')
+        t_content = self.t_content + f.read()
+        f.close()
+
+        return t_content
+
+    def GetImage(self):
+        f = open('arch.jpg','rb')
+        p_content = self.p_content + f.read()
+        f.close()
+
+        return p_content
+
+    def Get404Page(self):
+        f = open('404page.html','rb')
+        NotFoundContent= self.NotFoundContent + f.read()
+        f.close()
+        return NotFoundContent
+
+    def isIndexRequested(self,src):
+        if src == '/' or src == '/index.html':
+            return True
 
         else:
-            outputcontent=t_content
+            return False
 
-        connection.sendall(outputcontent)
+    def isImageRequested(self,src):
+        if src == '/arch.jpg':
+            return True
 
-    connection.close()
+        else:
+            return False
+
+
+
+server = socketserver.TCPServer(server_address,MYTCPHandler)
+
+server.serve_forever()
